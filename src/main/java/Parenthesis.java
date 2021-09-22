5df8c4b35e644e944243a3b7126c523206eb9c62
ekavtaradze <ekavtaradze@wpi.edu>
2021-09-22 05:51:02 +0400

    delete Parenthesis

--- src/main/java/Parenthesis.java
+++
@@ -1,9 +0,0 @@
-public class Parenthesis {
-    public char value;
-    public int index;
-
-    public Parenthesis(char value, int index) {
-        this.value = value;
-        this.index = index;
-    }
-}
